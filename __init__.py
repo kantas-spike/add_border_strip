495231136ef6466fe1fc4cdf987bf184f4423043
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-11-30 23:03:10 +0900

    feat: ラベルだけ持つパネルをサイドバーに表示

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import bpy
+from bpy.types import Context
 
 bl_info = {
     "name": "add border strip",
@@ -11,11 +12,36 @@ bl_info = {
 }
 
 
+class AddBorderPanel(bpy.types.Panel):
+    bl_label = "Add border strip"
+    bl_space_type = "SEQUENCE_EDITOR"
+    bl_region_type = "UI"
+    bl_category = "Add Border Strip"
+
+    @classmethod
+    def poll(cls, context):
+        return context.space_data.view_type == "SEQUENCER"
+
+    def draw_header(self, context):
+        layout = self.layout
+        layout.label(text="", icon="PLUGIN")
+
+    def draw(self, context: Context):
+        return super().draw(context)
+
+
+classList = [AddBorderPanel]
+
+
 def register():
+    for c in classList:
+        bpy.utils.register_class(c)
     print("add_border_strip: registered")
 
 
 def unregister():
+    for c in classList:
+        bpy.utils.unregister_class(c)
     print("add_border_strip: unregistered")
 
 
